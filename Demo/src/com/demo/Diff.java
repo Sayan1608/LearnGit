d1829fea07ffdc3fa6b8df7e3bb74ca6fb0218aa
Sayan Sengupta <subhosen16@gmail.com>
2023-03-11 13:16:07 +0530

    Changed Implementation of Diff.java

--- Demo/src/com/demo/Diff.java
+++ Demo/src/com/demo/Diff.java
@@ -1,12 +1,15 @@
 package com.demo;
 
+import java.util.Scanner;
+
 public class Diff {
 
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 20;
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
 		
-		System.out.println("Diff = " + (b-a));
+		System.out.println("Diff of "+a+" and "+b+" = " + (b-a));
 	}
 
 }
